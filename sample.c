ab4e40b306a2ca24dbccc1a8acc51fad67eabf9f
MUHAMMED YASEEN N A <36787696+121yaseen@users.noreply.github.com>
2019-02-27 20:07:46 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -3,5 +3,6 @@ int main()
 {
 printf("This is a test code by Yaseen");
 printf("After certain days for testing I edited and updated it");
+printf("This is the second time i an merging it");
 return 0;
 }
